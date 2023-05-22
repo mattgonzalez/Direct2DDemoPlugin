32deff91778c660a670884a59bb902804351b278
Matt Gonzalez <matt@echoaudio.com>
2023-05-21 20:28:06 -0700

    Added license text and copyright notice to all source files

--- Source/Direct2DAttachment.cpp
+++ Source/Direct2DAttachment.cpp
@@ -1,3 +1,27 @@
+/*
+
+Copyright(c) 2023 Matthew Gonzalez
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files(the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions :
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
 #if _WIN32
 #include <windows.h>
 #include <commctrl.h>
@@ -64,20 +88,14 @@ static LRESULT subclassProc(HWND hwnd, UINT umsg, WPARAM wParam, LPARAM lParam, 
         return 0;
     }
     case WM_ERASEBKGND:
-        //case WM_NCCALCSIZE:
         return 1;
 
-        //case WM_NCPAINT:
-        //case WM_WINDOWPOSCHANGING:
-          //  return 0;
     case WM_SIZING:
     {
         that->handleResize();
         break;
     }
     }
-
-    //DBG("umsg " << juce::String::toHexString(umsg));
 
     return DefSubclassProc(hwnd, umsg, wParam, lParam);
 }
@@ -170,11 +188,6 @@ void Direct2DAttachment::handleResize()
     }
 
     paintImmediately();
-#else
-    //
-    // Only works for Direct2D mode
-    //
-    jassertfalse;
 #endif
 }
 
